f79e6c3b2ba279836d240d8597c6732cacad7638
olusoji <amakafash20@gmail.com>
2025-11-25 20:12:17 +0000

    Fixed React style error, updated favicon paths, improved Netlify hidden form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,14 @@ function Footer() {
       <p>Custom websites built from code.</p>
 
       {/* CSS Validator Badge */}
-      <a
-        href="https://jigsaw.w3.org/css-validator/check/referer"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          style={{ border: 0, width: "88px", height: "31px", marginTop: "10px" }}
-          src="https://jigsaw.w3.org/css-validator/images/vcss"
-          alt="Valid CSS!"
-        />
-      </a>
+      <a href="https://jigsaw.w3.org/css-validator/validator?uri=https://www.codebuiltsites.com&profile=css3svg&usermedium=all&warning=1&vextwarning=">
+ <img
+  style={{ border: 0, width: "88px", height: "31px" }}
+  src="https://jigsaw.w3.org/css-validator/images/vcss"
+  alt="Valid CSS!"
+/>
+</a>
+
     </footer>
   );
 }
